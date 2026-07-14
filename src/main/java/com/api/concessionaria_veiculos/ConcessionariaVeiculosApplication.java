788d39a88e8c1b1e9bfb56b198cbeb54aa084793
CaioRian187 <caiorian246810@gmail.com>
2026-07-14 10:19:24 -0300

    Iniciando Projeto

---
+++ src/main/java/com/api/concessionaria_veiculos/ConcessionariaVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.api.concessionaria_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariaVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionariaVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/concessionaria_veiculos/ConcessionariaVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.concessionaria_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
