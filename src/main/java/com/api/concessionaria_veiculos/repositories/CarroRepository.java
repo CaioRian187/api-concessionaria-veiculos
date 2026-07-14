82e6e0edfbe53674b49c5624fa684eb6f5f1efa7
CaioRian187 <caiorian246810@gmail.com>
2026-07-14 13:27:55 -0300

    feature: Criando o CarroRepository

---
+++ src/main/java/com/api/concessionaria_veiculos/repositories/CarroRepository.java
@@ -0,0 +1,9 @@
+package com.api.concessionaria_veiculos.repositories;
+
+import com.api.concessionaria_veiculos.models.entities.CarroEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CarroRepository extends JpaRepository<CarroEntity, UUID> {
+}
